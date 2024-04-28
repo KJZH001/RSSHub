3c25615d3e2243f1cc66699485ebe1ccf8aaf71f
KJZH001 <2802007619@qq.com>
2024-04-28 22:04:46 +0800

    修改部分文本

--- lib/views/index.tsx
+++ lib/views/index.tsx
@@ -140,17 +140,17 @@ const Index: FC<{ debugQuery: string | undefined }> = ({ debugQuery }) => {
                 <h1 className="text-4xl font-bold">
                     Welcome to <span className="text-[#F5712C]">RSSHub</span>!
                 </h1>
-                <p className="text-zinc-500">If you see this page, the RSSHub is successfully installed and working.</p>
-                <p className="text-xl font-medium text-zinc-600">Everything is RSSible</p>
+                <p className="text-zinc-500">如果你看到这个页面，说明RSSHub已经在正常运行</p>
+                <p className="text-xl font-medium text-zinc-600">让任何站点可被rss订阅</p>
                 <div className="font-bold space-x-4 text-sm">
                     <a target="_blank" href="https://docs.rsshub.app">
-                        <button className="text-white bg-[#F5712C] hover:bg-[#DD4A15] py-2 px-4 rounded-full transition-colors">View Docs</button>
+                        <button className="text-white bg-[#F5712C] hover:bg-[#DD4A15] py-2 px-4 rounded-full transition-colors">阅读文档</button>
                     </a>
-                    <a target="_blank" href="https://github.com/DIYgod/RSSHub">
-                        <button className="bg-zinc-200 hover:bg-zinc-300 py-2 px-4 rounded-full transition-colors">View on GitHub</button>
+                    <a target="_blank" href="https://github.com/KJZH001/RSSHub">
+                        <button className="bg-zinc-200 hover:bg-zinc-300 py-2 px-4 rounded-full transition-colors">在Github查看</button>
                     </a>
                     <a target="_blank" href="https://docs.rsshub.app/sponsor" className="text-[#F5712C]">
-                        <button className="text-white bg-red-500 hover:bg-red-600 py-2 px-4 rounded-full transition-colors">❤️ Sponsor</button>
+                        <button className="text-white bg-red-500 hover:bg-red-600 py-2 px-4 rounded-full transition-colors">❤️ 赞助原作者</button>
                     </a>
                 </div>
                 {info.showDebug ? (
@@ -181,11 +181,11 @@ const Index: FC<{ debugQuery: string | undefined }> = ({ debugQuery }) => {
                     </a>
                 </p>
                 <p className="!mt-6">
-                    Please consider{' '}
+                    请支持并{' '}
                     <a target="_blank" href="https://docs.rsshub.app/sponsor" className="text-[#F5712C]">
-                        sponsoring
+                        赞助
                     </a>{' '}
-                    to help keep this open source project alive.
+                    这有助于推动RSSHub的发展和持续更新
                 </p>
                 <p>
                     Made with ❤️ by{' '}
@@ -196,7 +196,7 @@ const Index: FC<{ debugQuery: string | undefined }> = ({ debugQuery }) => {
                     <a target="_blank" href="https://github.com/DIYgod/RSSHub/graphs/contributors" className="text-[#F5712C]">
                         Contributors
                     </a>{' '}
-                    under MIT License.
+                    使用 MIT License 许可
                 </p>
             </div>
         </Layout>
